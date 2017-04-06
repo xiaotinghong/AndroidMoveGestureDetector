25f981e334801a4363bcaf2a39d11167cf2e5b68
ceciliahong <cecilia.x.hong@ibm.com>
2017-04-06 14:37:15 -0300

    Recover the code

--- MoveGestureDetector/app/src/main/java/com/example/xiaotinghong/movegesturedetector/MainActivity.java
+++ MoveGestureDetector/app/src/main/java/com/example/xiaotinghong/movegesturedetector/MainActivity.java
@@ -1,13 +1,104 @@
 package com.example.xiaotinghong.movegesturedetector;
 
+import android.graphics.Rect;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.LinkedList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
+    private static String TAG = "Move Gesture Detector";
+
+    List<ImageView> allImageViews;
+    private ImageView selectedImageView;
+
+    private MoveGestureDetector moveGestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add all the image view to the candidate list
+        allImageViews = new LinkedList<>();
+        allImageViews.add( (ImageView) findViewById(R.id.image_view_1) );
+        allImageViews.add( (ImageView) findViewById(R.id.image_view_2) );
+        allImageViews.add( (ImageView) findViewById(R.id.image_view_3) );
+
+        selectedImageView = null;
+
+        moveGestureDetector = new MoveGestureDetector(this, new MoveListener());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Let moveGestureDetector detects all events
+        if (moveGestureDetector != null) {
+            moveGestureDetector.onTouchEvent(event);
+        }
+
+        // Decide which image view should be moved based on events
+        updateSelectedImageView(event);
+
+        return true;
+    }
+
+    private void updateSelectedImageView(MotionEvent event) {
+        final int action = MotionEventCompat.getActionMasked(event);
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN: {
+                selectedImageView = selectImageViewOnEvent(event);
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                selectedImageView = null;
+                break;
+            }
+        }
+    }
+
+    private ImageView selectImageViewOnEvent (MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        for(ImageView imageView: allImageViews) {
+            // Calculate the hit rect of the current image view
+            int[] screenLocation = new int[2];
+            imageView.getLocationOnScreen(screenLocation);
+            Rect hitRect = new Rect(screenLocation[0], screenLocation[1],
+                    screenLocation[0] + imageView.getWidth(), screenLocation[1] + imageView.getHeight());
+
+            // check if the touch point land within the hit rect
+            if( hitRect.contains((int) touchX, (int) touchY) ) {
+                return imageView;
+            }
+        }
+
+        return null;
+    }
+
+    private class MoveListener extends MoveGestureDetector.SimpleOnMoveGestureListener {
+        @Override
+        public boolean onMove(MoveGestureDetector detector) {
+            // The operations based on move touch event go here
+
+            if(selectedImageView == null) {
+                return false;
+            }
+
+            float deltaX = detector.getDeltaX();
+            float deltaY = detector.getDeltaY();
+
+            selectedImageView.setX(selectedImageView.getX() + deltaX);
+            selectedImageView.setY(selectedImageView.getY() + deltaY);
+
+            return true;
+        }
     }
 }
+
